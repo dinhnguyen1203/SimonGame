12c0d8738c172133b727b989673173f1c6e11fe6
dinhnguyen1203 <72590135+dinhnguyen1203@users.noreply.github.com>
2021-08-17 17:27:59 +0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,102 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.round(Math.random() * 3);
+
+  //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
+  var randomChosenColour = buttonColours[randomNumber];
+
+  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function playSound(colour) {
+  var audio = new Audio("sounds/" + colour + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(() => {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+$(document).keypress(function (event) {
+  if (started === false) {
+    nextSequence();
+    started = true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    startOver();
+  }
+}
+
+function startOver() {
+  console.log("wrong");
+  $("body").addClass("game-over");
+
+  setTimeout(() => {
+    $("body").removeClass("game-over");
+  }, 200);
+
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+  started = false;
+}
+
+$("#button-start").click(function () {
+  if (started === false) {
+    nextSequence();
+    started = true;
+  }
+});
+
+$("#button-refresh").click(function () {
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+  started = false;
+  nextSequence();
+  started = true;
+});
